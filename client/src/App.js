c5dca956d00d36e89393c0f124373cd8c530eaa2
aeonyuonmiller <aym1@mail.com>
2022-01-26 00:57:21 +0100

    styling optimization hero

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import { motion } from "framer-motion";
 import { useState, createContext } from "react";
 import { AnimatePresence } from "framer-motion";
 import { Footer } from "./components/Hero/Footer";
@@ -6,6 +7,11 @@ import { Hero } from "./components/Hero/Hero";
 import axios from "axios";
 
 export const AppContext = createContext(null);
+
+const speed = {
+  hidden: { opacity: 0 },
+  enter: { opacity: 1, rotate: 360 },
+};
 
 // fetch of mentors collection
 async function getMentors() {
@@ -18,19 +24,45 @@ async function getMentors() {
 }
 
 function App() {
+  const container = {
+    hidden: { opacity: 0 },
+    show: {
+      opacity: 1,
+      transition: {
+        type: "tween",
+        staggerChildren: 0.1,
+      },
+    },
+  };
+
+  const item = {
+    hidden: { opacity: 0, y: 20 },
+    show: { opacity: 1, y: 0 },
+  };
+
   return (
     <AppContext.Provider value={{}}>
       <AnimatePresence>
         <div className="bg" />
         <nav>
-          <ul>
-            <li>Mixes</li>
-            <li>Mentors</li>
-            <li>Shop</li>
-            <li className="loginBtn">Login</li>
-          </ul>
+          <motion.ul variants={container} initial="hidden" animate="show">
+            <motion.li variants={item}>Mixes</motion.li>
+            <motion.li variants={item}>Mentors</motion.li>
+            <motion.li variants={item}>Shop</motion.li>
+            <motion.li variants={item} className="loginBtn">
+              Login
+            </motion.li>
+          </motion.ul>
         </nav>
-        <img src="/logo.png" className="logo" alt="Femme Bass Mafia" />
+        <motion.img
+          drag
+          initial="hidden"
+          animate="enter"
+          variants={speed}
+          src="/logo.png"
+          className="logo"
+          alt="Femme Bass Mafia"
+        />
         <Hero />
         <div className="content">
           <h2>Roboto Typeface</h2>
--- client/src/components/Hero/Hero.jsx
+++ client/src/components/Hero/Hero.jsx
@@ -1,13 +1,36 @@
 import React from 'react'
 import './Hero.css'
+import { motion, useViewportScroll, useTransform } from 'framer-motion'
+
+const swipe = {
+    hidden: {opacity:0, y:-30},
+    enter: {opacity:1, y:0, transition:{ delay:0.6, }},
+    exit: {opacity:0, y:-20 }
+};
 
 export const Hero = () => {
+
+    const { scrollYProgress } = useViewportScroll()
+    const yLength = useTransform(scrollYProgress, [0, 1], [0, -100]);
+    const yMinusLength = useTransform(scrollYProgress, [0, .8], [0, 200]);
+    const trans = useTransform(scrollYProgress, [0, 1], [1, -1]);
+    const size = useTransform(scrollYProgress, [0, 1], [1, 1.1]);
+    
     return (
         <div className="hero">
             {/* gfx-in-css */}
-            <div className="pattern" />
+            <motion.div
+                className="pattern" 
+                initial="hidden"
+                animate="enter"
+                style={{ scale:size, y:yMinusLength }}
+                variants={swipe} />
             <div className="hero-content">
-                <h1><strong>FemmeBassMafia</strong><span className="acronym">FBM</span> offers <span>mentorship</span> dedicated to women, transgender and non-binary people to learn <span>DJing</span></h1>
+                <motion.h1
+                    initial="hidden"
+                    animate="enter"
+                    style={{y:yLength, opacity:trans}}
+                    variants={swipe}><strong>FemmeBassMafia</strong><span className="acronym">FBM</span> offers <span>mentorship</span> dedicated to women, transgender and non-binary people to learn <span>DJing</span></motion.h1>
             </div>
         </div>
     )
