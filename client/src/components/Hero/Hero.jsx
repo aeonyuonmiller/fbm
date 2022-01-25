5af91de2178b55d8b684375808c5363b620a51fe
aeonyuonmiller <aym1@mail.com>
2022-01-25 23:06:34 +0100

    styling hero

--- client/src/components/Hero/Hero.jsx
+++ client/src/components/Hero/Hero.jsx
@@ -6,8 +6,8 @@ export const Hero = () => {
         <div className="hero">
             {/* gfx-in-css */}
             <div className="pattern" />
-            <div className="content">
-                <h1><strong>FemmeBassMafia</strong> offers <span>mentorship</span> dedicated to women, transgender and non-binary people to learn <span>DJing</span></h1>
+            <div className="hero-content">
+                <h1><strong>FemmeBassMafia</strong><span className="acronym">FBM</span> offers <span>mentorship</span> dedicated to women, transgender and non-binary people to learn <span>DJing</span></h1>
             </div>
         </div>
     )
