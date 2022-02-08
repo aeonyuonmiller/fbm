558112c6e07d93b0eed569a265472873c6df2b8e
aeonyuonmiller <aym1@mail.com>
2022-02-08 15:06:16 +0100

    refactored exit animation

--- client/src/components/LoginModal/LoginModal.jsx
+++ client/src/components/LoginModal/LoginModal.jsx
@@ -6,7 +6,7 @@ import './LoginModal.css'
     const container = {
         show: { opacity: 1, scale: 1, x: 0, transition:{ease: [0.9, 0, 0.1, 1], duration: 1}},
         hidden: { opacity: 0, scale: 0.9, x: 200 },
-        exit: { opacity: 0, x: 200 }
+        exit: { opacity: 0, x: 300, transition:{type:"tween", duration: .6}}
     }
     const input = {
         show: { opacity: 1, transition:{ staggerChildren: 0.3, duration: .8, delay: 0.4 }},
@@ -30,13 +30,14 @@ const LoginModal = ({closeModal}) => {
                 variants={container}
                 initial="hidden"
                 animate="show"
+                exit="exit"
                 style={{ originX: 1 }}>
                 <h5>Login</h5>
                 <motion.input variants={input} initial="hidden" animate="show" type="text" placeholder="Username" />
                 <motion.input variants={input} initial="hidden" animate="show" type="email" placeholder="Email" />
                 <motion.input variants={input} initial="hidden" animate="show" type="password" placeholder="Password" />
-                <motion.button exit={{y:100}} initial={{y:100}} animate={{y:0, transition:{delay:.5, duration: .8, ease:[0.9, 0, 0.1, 1]}}} whileHover={{ scale:1.03 }}>Login</motion.button>
-                <motion.button exit={{y:100}} initial={{y:100}} animate={{y:0, transition:{delay:.65, duration: .8, ease:[0.9, 0, 0.1, 1]}}} whileHover={{ scale:1.03 }} className="secondary">Sign Up</motion.button>
+                <motion.button initial={{y:100}} animate={{y:0, transition:{delay:.5, duration: .8, ease:[0.9, 0, 0.1, 1]}}} whileHover={{ scale:1.03 }}>Login</motion.button>
+                <motion.button initial={{y:100}} animate={{y:0, transition:{delay:.65, duration: .8, ease:[0.9, 0, 0.1, 1]}}} whileHover={{ scale:1.03 }} className="secondary">Sign Up</motion.button>
             </motion.form>
             <motion.div
                 className="backdrop"
