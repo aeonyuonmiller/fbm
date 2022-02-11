35b9afe0d330b72e79553c36a24420a6aa1db7ce
aeonyuonmiller <aym1@mail.com>
2022-02-11 14:45:34 +0100

    added refs and states

--- client/src/components/LoginModal/SignUpModal.jsx
+++ client/src/components/LoginModal/SignUpModal.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import { Link, Routes, Route } from "react-router-dom";
 import { motion, AnimatePresence } from "framer-motion";
 import LoginModal from "./LoginModal";
@@ -19,7 +19,19 @@ import './LoginModal.css'
         hidden: { opacity: 0 }
     }
 
-const SignUpModal = ({closeModal}) => {
+const SignUpModal = ({ closeModal }) => {
+    
+    // useStates
+    const [fullName, setFullName] = useState("");
+    const [username, setUsername] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    
+    // useRefs
+    const fullNameRef = useRef();
+    const usernameRef = useRef();
+    const emailRef = useRef();
+    const passwordRef = useRef();
 
     return <>
             <motion.form layoutId="form" className="login"
@@ -29,10 +41,10 @@ const SignUpModal = ({closeModal}) => {
                 exit="exit"
                 style={{ originX: 1 }}>
                 <h5>Sign Up</h5>
-                <motion.input variants={input} initial="hidden" animate="show" type="text" placeholder="Full name" />
-                <motion.input variants={input} initial="hidden" animate="show" type="text" placeholder="Username" />
-                <motion.input variants={input} initial="hidden" animate="show" type="email" placeholder="Email" />
-                <motion.input variants={input} initial="hidden" animate="show" type="password" placeholder="Password" />
+                <motion.input value={fullName} ref={fullNameRef} variants={input} initial="hidden" animate="show" type="text" placeholder="Full name" />
+                <motion.input value={username} ref={usernameRef} variants={input} initial="hidden" animate="show" type="text" placeholder="Username" />
+                <motion.input value={email} ref={emailRef} variants={input} initial="hidden" animate="show" type="email" placeholder="Email" />
+                <motion.input value={password} ref={passwordRef} variants={input} initial="hidden" animate="show" type="password" placeholder="Password" />
                 <motion.button initial={{y:100}} animate={{y:0, transition:{delay:.5, duration: .6, ease:[0.9, 0, 0.1, 1]}}} whileHover={{ scale:1.03 }}>Sign Up</motion.button>
                 <Link to="/"><motion.button initial={{y:100}} animate={{y:0, transition:{delay:.65, duration: .6, ease:[0.9, 0, 0.1, 1]}}} whileHover={{ scale:1.03 }} className="secondary">Login</motion.button></Link>
             </motion.form>
