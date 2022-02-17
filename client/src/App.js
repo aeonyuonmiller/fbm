d546f7a98d9e2e614c5f2986eff171897b8dc446
aeonyuonmiller <aym1@mail.com>
2022-02-17 20:01:57 +0100

    removed unused

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,6 @@
 import "./App.css";
 // import { useState } from "react";
 import { motion, MotionConfig } from "framer-motion";
-// import { Link } from "react-router-dom";
-// import { Footer } from "./components/Footer/Footer";
-// import { Hero } from "./components/Hero/Hero";
-// import MentorList from "./components/MentorList/MentorList";
 import Nav from "./components/Nav/Nav";
 import { MentorsContextProvider } from "./Context/mentorsContext";
 
